ef7eaf8b5920c05c9d9444b1ffb8dfcecf6d423c
Matthew Chenette <matthew@chenette.com>
2024-01-18 20:06:00 -0600

    testing with new service account

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "random_id" "bucket_prefix" {
 }
 
 resource "google_storage_bucket" "static_website" {
-  name          = "${random_id.bucket_prefix.hex}-static-website-bucket"
+  name          = "${random_id.bucket_prefix.hex}-static-website-bucket-2"
   location      = "US"
   storage_class = "STANDARD"
   website {
@@ -48,4 +48,61 @@ resource "google_storage_bucket_access_control" "public_rule" {
   bucket = google_storage_bucket.static_website.id
   role   = "READER"
   entity = "allUsers"
+}
+
+
+
+
+//////////////////////////////////////////
+
+locals {
+  project_id   = "chenette" //"my-google-project"
+  organization = "chenette" //"my-github-organization"
+  repo         = "chenette.com" //"my-github-repository"
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  project                   = local.project_id
+  workload_identity_pool_id = "github-pool"
+  display_name              = "GitHub pool"
+  description               = "Identity pool for GitHub deployments"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = local.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
+    "attribute.repository" = "assertion.repository"
+  }
+
+  attribute_condition = "assertion.repository_owner==\"${local.organization}\""
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account" "github_actions" {
+  project      = local.project_id
+  account_id   = "github-actions"
+  display_name = "Service Account used for GitHub Actions"
+}
+
+resource "google_service_account_iam_member" "workload_identity_user" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.organization}/${local.repo}"
+}
+
+output "workload_identity_provider" {
+  value = "${google_iam_workload_identity_pool.github_pool.name}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
+}
+
+output "service_account" {
+  value = google_service_account.github_actions.email
 }
